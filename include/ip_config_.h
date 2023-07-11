edf2b7f432a1ab47d0c38a0f947fe6f8080cb4c2
Shim Hun <shimhun88@naver.com>
2023-07-11 16:29:01 +0900

    .gitignore

--- include/ip_config_.h
+++ include/ip_config_.h
@@ -1,5 +1,5 @@
-// KETI Storage Engine Instance 내부 포트
-#define LOCALHOST "10.0.4.82"
+// KETI 개발 Storage Engine Instance 내부 포트
+#define LOCALHOST "xx.xx.xx.xx"
 
 // Storage Engine Node :: Interface Container
 #define SE_INTERFACE_CONTAINER_PORT 40200
@@ -15,13 +15,13 @@
 #define SE_MONITORING_CONTAINER_PORT 40203
 
 // Storage Node :: CSD Proxy
-#define CSD_PROXY_IP "10.0.4.83"
+#define CSD_PROXY_IP "xx.xx.xx.xx"
 #define CSD_PROXY_PORT 40300
 
 // Storage Node :: LBA2PBA Manager
-#define CSD_LBA2PBA_IP "10.0.4.83"
+#define CSD_LBA2PBA_IP "xx.xx.xx.xx"
 #define CSD_LBA2PBA_PORT 40301
 
 // Storage Node :: WAL Manager
-#define CSD_WAL_MANAGER_IP "10.0.4.83"
+#define CSD_WAL_MANAGER_IP "xx.xx.xx.xx"
 #define CSD_WAL_MANAGER_PORT 40302
