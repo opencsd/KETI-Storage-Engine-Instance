1ca469812f414b229ba105aba71579f2ec359c5f
juhyunkil <wngus0331@gmail.com>
2023-08-29 10:46:12 +0900

    main.cc interface

--- interfacecontainer/main.cc
+++ interfacecontainer/main.cc
@@ -3,10 +3,14 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <arpa/inet.h>
 
 #include "ip_config.h"
 #include "SnippetManager.h"
-#include "CalculateUsage.h"
 
 #include <grpcpp/grpcpp.h>
 #include "storage_engine_instance.grpc.pb.h"
@@ -24,13 +28,51 @@ using StorageEngineInstance::Request;
 
 using namespace std;
 
+void SendQueryStatus(const char* message){
+  int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        std::cerr << "Socket creation error" << std::endl;
+        // return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        // return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        std::cerr << "Connection Failed" << std::endl;
+        // return -1;
+    }
+
+    // Send message to the server
+    send(sock, message, strlen(message), 0);
+    std::cout << "Query info sent" << std::endl;
+
+    // Receive message from the server
+    valread = read(sock, buffer, 1024);
+    std::cout << buffer << std::endl;
+}
+
 // Logic and data behind the server's behavior.
 class InterfaceContainerServiceImpl final : public InterfaceContainer::Service {
-  Status SetSnippet(ServerContext* context,
-                   ServerReaderWriter<Result, SnippetRequest>* stream) override {
-    // CalculateStart();
+  Status SetSnippet(ServerContext* context, ServerReaderWriter<Result, SnippetRequest>* stream) override {
     SnippetRequest snippet_request;
     bool flag = true;
+
+    if(KETILOG::IsLogLevel(METRIC)){
+      const char* Qstart = "Query Start";
+      SendQueryStatus(Qstart);
+    }
 
     while (stream->Read(&snippet_request)) {      
       if(flag){
@@ -63,7 +105,6 @@ class InterfaceContainerServiceImpl final : public InterfaceContainer::Service {
     string table_name = SnippetManager::GetTableName(query_id);
 
     KETILOG::INFOLOG("Interface Container", "==:RUN:== {" + to_string(query_id) + "}");
-    // sleep(1+rand()%5);
 
     Merging_Container_Interface mc(grpc::CreateChannel((std::string)LOCALHOST+":"+std::to_string(SE_MERGING_CONTAINER_PORT), grpc::InsecureChannelCredentials()));
     string query_result = mc.GetQueryResult(query_id, table_name);
@@ -72,11 +113,68 @@ class InterfaceContainerServiceImpl final : public InterfaceContainer::Service {
 
     SnippetManager::EraseQueryID(query_id); //이후 캐시 사용시 고려 필요
     mc.EndQuery(query_id);
-    // CalculateEnd();
+
+    if(KETILOG::IsLogLevel(METRIC)){
+      const char* Qend = "Query End";
+      SendQueryStatus(Qend);
+    }
+
+    return Status::OK;
+  }
+
+  Status SetSnippetAndRun(ServerContext* context, grpc::ServerReader<SnippetRequest>* reader, Result* response) override {
+    SnippetRequest snippet_request;
+    bool flag = true;
+
+    if(KETILOG::IsLogLevel(METRIC)){
+      const char* Qstart = "Query Start";
+      SendQueryStatus(Qstart);
+    }
+
+    while (reader->Read(&snippet_request)) {      
+      if(flag){
+        string msg = "==:Set Snippet:== {" + to_string(snippet_request.snippet().query_id()) + "}";
+        KETILOG::INFOLOG("Interface Container", msg);
+        SnippetManager::InsertQueryID(snippet_request.snippet().query_id());
+        flag = false;
+      } 
+
+      // Check Recv Snippet
+      {
+        // std::string test_json;
+        // google::protobuf::util::JsonPrintOptions options;
+        // options.always_print_primitive_fields = true;
+        // options.always_print_enums_as_ints = true;
+        // google::protobuf::util::MessageToJsonString(snippet_request,&test_json,options);
+        // std::cout << endl << test_json << std::endl << std::endl; 
+      }
+
+      SnippetManager::PushQueue(snippet_request);
+    }
+
+    SnippetManager::SetTableName(snippet_request.snippet().query_id(), snippet_request.snippet().table_alias());
+
+    int query_id = snippet_request.snippet().query_id();
+    string table_name = SnippetManager::GetTableName(query_id);
+
+    KETILOG::INFOLOG("Interface Container", "==:RUN:== {" + to_string(query_id) + "}");
+
+    Merging_Container_Interface mc(grpc::CreateChannel((std::string)LOCALHOST+":"+std::to_string(SE_MERGING_CONTAINER_PORT), grpc::InsecureChannelCredentials()));
+    string query_result = mc.GetQueryResult(query_id, table_name);
+    
+    response->set_value(query_result);
+
+    SnippetManager::EraseQueryID(query_id); //이후 캐시 사용시 고려 필요
+    mc.EndQuery(query_id);
+
+    if(KETILOG::IsLogLevel(METRIC)){
+      const char* Qend = "Query End";
+      SendQueryStatus(Qend);
+    }
+
     return Status::OK;
   }
 };
-
 
 void RunServer() {
   std::string server_address((std::string)LOCALHOST+":"+std::to_string(SE_INTERFACE_CONTAINER_PORT));
